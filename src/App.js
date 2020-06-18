bca96153aebcca9426966405ed6f27c1481c7df8
Max <blancarte@gmail.com>
2020-06-18 14:18:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//state del formulario
+
+	const [busqueda, guardarBusqueda] = useState({
+		ciudad: "",
+		pais: "",
+	});
+
+	const [consultar, guardarConsultar] = useState(false);
+	const [resultado, guardarResultado] = useState({});
+	const [error, guardarError] = useState(false);
+
+	const { ciudad, pais } = busqueda;
+
+	useEffect(() => {
+		if (consultar) {
+			const consultarApi = async () => {
+				const appID = "9097fa08e11c199f71ff8bb470aa859a";
+				const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+
+				const respuesta = await fetch(url);
+				const resultado = await respuesta.json();
+				guardarResultado(resultado);
+				guardarConsultar(false);
+
+				if (resultado.cod === "404") {
+					guardarError(true);
+				} else {
+					guardarError(false);
+				}
+			};
+
+			consultarApi();
+		}
+	}, [consultar, pais, ciudad]);
+
+	let componente;
+
+	if (error) {
+		componente = <Error mensaje='No hay resultados' />;
+	} else {
+		componente = <Clima resultado={resultado} />;
+	}
+
+	return (
+		<Fragment>
+			<Header titulo='Clima React App' />
+			<div className='contenedor-form'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col m6 s12'>
+							<Formulario
+								busqueda={busqueda}
+								guardarBusqueda={guardarBusqueda}
+								guardarConsultar={guardarConsultar}
+							/>
+						</div>
+						<div className='col m6 s12'>{componente}</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
